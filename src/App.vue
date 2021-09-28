a1e38d57db46fe870281d6bba26b7b38399d7dda
marianaolvr <mariana.oliveira@sinch.com>
2021-09-27 23:25:06 -0300

    feat: add a background and the base for the card component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="title">
+      <h1>COMPUTER QUIZ</h1>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+  border: 0;
+  box-sizing: border-box;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  background-image: url('../src/assets/computer-image.jpg');
+  background-size: 100% 100%;
+  height: 100vh;
+}
+
+.title {
+  padding-top: 21px;
   text-align: center;
+  font-family: 'Staatliches', cursive;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/views/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="index">
+    <QuizCard />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import QuizCard from '@/components/QuizCard.vue'
+
+export default Vue.extend({
+  name: 'Index',
+  components: {
+    QuizCard
+  }
+})
+</script>
